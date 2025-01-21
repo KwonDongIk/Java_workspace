0c71f7ec6083c51f411104d3d9f96781fb9192d9
KwonDongIk <dongikik@kakao.com>
2025-01-22 00:33:27 +0900

    0122 AM 12:33 commit

--- HelloWorld/src/com/yedam/Todo.java
+++ HelloWorld/src/com/yedam/Todo.java
@@ -7,6 +7,8 @@ public class Todo {
 		Scanner scn = new Scanner(System.in);
 		
 		int balance = 0; // 예금액
+		int sum = 0; // 예금액 중첩 sum 선언
+		int withdraw = 0; // 출금액
 		
 		while(true) {
 		System.out.println("1. 입금 2. 출금 3. 잔액조회 4. 종료 ");
@@ -17,33 +19,33 @@ public class Todo {
 		
 		if (num == 1) {
 			
-			// 입금
-			System.out.println("금액을 입력하세요.>");
+			// 1. 입금
+			System.out.println("입금할 금액을 입력하세요.>");
 			balance = Integer.parseInt(scn.nextLine());
+			sum += balance;
+			System.out.println("입금 완료");
 			
 		} else if (num == 2) {
 			
-			// 출금
+			// 2. 출금
+			System.out.println("출금할 금액을 입력하세요.>");
+			withdraw = Integer.parseInt(scn.nextLine());
+			sum -= withdraw;
+			System.out.println("출금 완료");
 			
 		} else if (num == 3) {
 			
-			// 잔액조회
-			System.out.printf("현재 잔액은 %d입니다.\n", balance);
+			// 3. 잔액조회
+			System.out.printf("현재 잔액은 %d원입니다.\n", sum);
 			
 		} else {
 			
-			// 종료
-			System.out.println("종료하고 처음으로 돌아갑니다.");
-			break;
-			
-			
+			// 4. 종료
+			System.out.println("종료합니다.");
+					
 		}
-		
-		
-		System.out.println("end of program");
+		System.out.println("처음화면으로 돌아갑니다.\n");
 		//System.out.printf("입력한 문자는 %s", str);
 		}
-		
 	}
-
 }
